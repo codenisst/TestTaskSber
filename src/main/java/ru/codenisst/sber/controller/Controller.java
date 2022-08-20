857aebf5952e231f20539133b05bf80f77f8ae7e
codenisst <yabangi.egor@ya.ru>
2022-08-21 04:12:29 +0500

    Added default controller

---
+++ src/main/java/ru/codenisst/sber/controller/Controller.java
@@ -0,0 +1,13 @@
+package ru.codenisst.sber.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @PostMapping
+    public void test() {
+
+    }
+}
