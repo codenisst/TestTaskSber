bdd7b39571da3a0fbd1e38b85c49df0d61c49bd0
codenisst <yabangi.egor@ya.ru>
2022-08-21 04:09:37 +0500

    Integrated database

---
+++ src/main/java/ru/codenisst/sber/TestTaskSberApplication.java
@@ -0,0 +1,13 @@
+package ru.codenisst.sber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskSberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskSberApplication.class, args);
+    }
+
+}
